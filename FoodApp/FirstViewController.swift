cd2e4ac477c549a933d6dfd5150ed00d104d68d0
SamuelMrejen <samuel.mrejen@ynov.com>
2018-11-06 14:25:59 +0100

    Initial Commit

---
+++ FoodApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FoodApp
+//
+//  Created by Mrejen on 06/11/2018.
+//  Copyright © 2018 Ynov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
