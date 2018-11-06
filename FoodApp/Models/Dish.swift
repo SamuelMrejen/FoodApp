73e8eed0144a6873db66325d8e990a39cfa5ed0f
SamuelMrejen <samuel.mrejen@ynov.com>
2018-11-06 14:50:24 +0100

    Add Menu and Dish models

---
+++ FoodApp/Models/Dish.swift
@@ -0,0 +1,17 @@
+//
+//  Dish.swift
+//  FoodApp
+//
+//  Created by Mrejen on 06/11/2018.
+//  Copyright © 2018 Ynov. All rights reserved.
+//
+
+import Foundation
+
+struct Dish{
+    var name:String
+    var price : Double
+    var picture: String
+    var desription:String
+    
+}
